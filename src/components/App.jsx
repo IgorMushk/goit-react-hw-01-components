f3f896856ef2672d42d87ea5d5fc64a2ceb23571
IgorMushk <igor.mushketik@gmail.com>
2023-08-31 17:53:01 +0300

    Install styled-components - App.js  styped

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import user from '../data/user.json';
+import { Container } from './App.styled';
+
 console.log(user);
 
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Container>React homework template</Container>;
 };
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
