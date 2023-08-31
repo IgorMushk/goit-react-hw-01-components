eb8e1b3593fe13f16b6ddb860a19e0c78a9f2362
IgorMushk <igor.mushketik@gmail.com>
2023-08-31 18:47:26 +0300

    Add component Profile - user.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,38 @@
 import user from '../data/user.json';
 import { Container } from './App.styled';
+import { Profile } from './Profile/Profile';
 
 console.log(user);
 
 export const App = () => {
-  return <Container>React homework template</Container>;
+  //return <Container>React homework template</Container>;
+  return (
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </Container>
+  );
 };
+
+//  "username": "Jacques Gluke",
+//   "tag": "jgluke",
+//   "location": "Ocho Rios, Jamaica",
+//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+
+// username — имя пользователя
+// tag — тег в социальной сети без @
+// location — город и страна
+// avatar — ссылка на изображение
+// stats — объект с информацией об активности
 
 // export const App = () => {
 //   return (
